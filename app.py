074ee829b38243a208ff2aef13852b976c9f8c55
Swarup Sahoo <swarupsahoo@utexas.edu>
2017-07-15 14:50:12 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/doggles/uploads'
+UPLOAD_FOLDER = '/uploads'
 #ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
