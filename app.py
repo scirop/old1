f8db4e3c04f6b10a3c01f9af3d646c829af66d0a
Swarup Sahoo <swarupsahoo@utexas.edu>
2017-07-15 14:59:34 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -49,3 +49,7 @@ from flask import send_from_directory
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
